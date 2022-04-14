67d66f2aaa7d2d582c45e49de541a13204555f29
Chibu <atuchibueze@gmail.com>
2022-04-14 09:48:21 -0400

    setup user input and process exit

--- play.js
+++ play.js
@@ -1,3 +1,25 @@
+const { stderr } = require('process');
 const { connect } = require('./client');
 
 connect();
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data', (key) => {
+    if (key === "\u0003") {
+      process.exit();
+    }
+  });
+  return stdin;
+};
+
+// const handleUserInput = function (key) {
+//   if (key === "\u0003") {
+//     process.exit();
+//   }
+// }
+
+setupInput();
